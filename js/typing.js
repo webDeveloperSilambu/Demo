d029311e89a692ca643b188121dcd97284be375f
DeveloperSilambu <97620286+webDeveloperSilambu@users.noreply.github.com>
2023-09-06 20:28:10 +0530

    Add files via upload

---
+++ js/typing.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#typing',{
+    strings : ["dance lover","web developer","app developer","CyberSecurity Researcher"],
+    typeSpedd : 50,
+    BackSpedd : 60,
+    loop : true
+})
